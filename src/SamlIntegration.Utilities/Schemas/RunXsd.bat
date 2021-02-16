81523735aa3e11138fa15999ea9ff0386c0d9ddb
Anton Yarkov <ayarkov@accesssoftek.com>
2021-02-16 16:35:34 +0300

    Minor edit

--- src/SamlIntegration.Utilities/Schemas/RunXsd.bat
+++ src/SamlIntegration.Utilities/Schemas/RunXsd.bat
@@ -1,1 +1,1 @@
-xsd xmldsig-core-schema.xsd xenc-core-schema.xsd saml-schema-assertion-2.0.xsd saml-schema-protocol-2.0.xsd saml-schema-metadata-2.0.xsd /classes /namespace:AccessSoftek.Common.Saml20 /outputdir:..
+xsd xmldsig-core-schema.xsd xenc-core-schema.xsd saml-schema-assertion-2.0.xsd saml-schema-protocol-2.0.xsd saml-schema-metadata-2.0.xsd /classes /namespace:SamlIntegration.Utilities /outputdir:..
